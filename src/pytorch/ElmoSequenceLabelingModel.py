d13e64b718182ec3158e46dbd0f79fc6ac838e07
keigotak <keigotak@localhost.com>
2019-12-31 10:48:17 +0900

    refactor: delete white line

--- src/pytorch/ElmoSequenceLabelingModel.py
+++ src/pytorch/ElmoSequenceLabelingModel.py
@@ -30,6 +30,7 @@ class ElmoSequenceLabelingModel(Model):
         if device != 'cpu':
             torch.cuda.manual_seed(seed)
             torch.cuda.manual_seed_all(seed)
+
         torch.backends.cudnn.enabled = False
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
