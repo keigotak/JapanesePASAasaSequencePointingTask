3122e64617572d54b24e5b8726061f6a4529cc94
keigotak <keigotak@localhost.com>
2020-01-02 10:49:08 +0900

    feat: add argument parser

--- src/pytorch/Orchestration.py
+++ src/pytorch/Orchestration.py
@@ -1,12 +1,18 @@
 import subprocess
 import os
 from concurrent import futures
+import argparse
 
-device = "3,5"
+parser = argparse.ArgumentParser(description='PASA Orchestration')
+parser.add_argument('--device', default=None, type=str)
+parser.add_argument('--num_worker', default=1, type=int)
+arguments = parser.parse_args()
+
+device = arguments.device
 if device != 'cpu':
     os.environ["CUDA_VISIBLE_DEVICES"] = device
 
-base_list = ["--model", "elmosl",
+base_list = ["--model", "bertsp",
              "--epochs", "20",
              "--max_eval", "38",
              "--earlystop", "5",
@@ -24,18 +30,17 @@ processes = [
     ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "1"],
     ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "2"],
     ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "3"],
-    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "4"],
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "4"]
 
-    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "0"],
-    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "1"],
-    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "2"],
-    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "3"],
-    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "4"]
+    # ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "0"],
+    # ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "1"],
+    # ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "2"],
+    # ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "3"],
+    # ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "4"]
 
 ]
 
-with futures.ProcessPoolExecutor(max_workers=8) as executor:
+with futures.ProcessPoolExecutor(max_workers=arguments.device) as executor:
     results = executor.map(subprocess.call, processes)
-
 
 print('completed.')
