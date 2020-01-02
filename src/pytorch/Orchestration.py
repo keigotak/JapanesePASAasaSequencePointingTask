59d24441926c74b6c88b4cf7d3b1c6ea12eaae87
keigotak <keigotak@localhost.com>
2020-01-02 10:26:07 +0900

    feat: add bert models

--- src/pytorch/Orchestration.py
+++ src/pytorch/Orchestration.py
@@ -2,20 +2,39 @@ import subprocess
 import os
 from concurrent import futures
 
-device = "1"
+device = "3,5"
 if device != 'cpu':
     os.environ["CUDA_VISIBLE_DEVICES"] = device
 
-base_list = ["--model", "elmosl", "--epochs", "20", "--max_eval", "38", "--embed", "glove-retrofitting", "--earlystop", "5", "--save_model", "--spreadsheet", "--line"]
+base_list = ["--model", "elmosl",
+             "--epochs", "20",
+             "--max_eval", "38",
+             "--earlystop", "5",
+             "--save_model",
+             "--spreadsheet",
+             "--line"]
 processes = [
-    ["python", "SequenceLabelingElmo.py"] + base_list + ["--device", device, "--seed", "0"],
-    ["python", "SequenceLabelingElmo.py"] + base_list + ["--device", device, "--seed", "1"],
-    ["python", "SequenceLabelingElmo.py"] + base_list + ["--device", device, "--seed", "2"],
-    ["python", "SequenceLabelingElmo.py"] + base_list + ["--device", device, "--seed", "3"],
-    ["python", "SequenceLabelingElmo.py"] + base_list + ["--device", device, "--seed", "4"]
+    # ["python", "SequenceLabelingElmo.py", "--embed", "glove-retrofitting"] + base_list + ["--device", device, "--seed", "0"],
+    # ["python", "SequenceLabelingElmo.py", "--embed", "glove-retrofitting"] + base_list + ["--device", device, "--seed", "1"],
+    # ["python", "SequenceLabelingElmo.py", "--embed", "glove-retrofitting"] + base_list + ["--device", device, "--seed", "2"],
+    # ["python", "SequenceLabelingElmo.py", "--embed", "glove-retrofitting"] + base_list + ["--device", device, "--seed", "3"],
+    # ["python", "SequenceLabelingElmo.py", "--embed", "glove-retrofitting"] + base_list + ["--device", device, "--seed", "4"]
+
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "0"],
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "1"],
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "2"],
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "3"],
+    ["python", "SequencePointingBert.py", "--decode", "ordered"] + base_list + ["--device", device, "--seed", "4"],
+
+    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "0"],
+    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "1"],
+    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "2"],
+    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "3"],
+    ["python", "SequencePointingBert.py", "--decode", "global_argmax"] + base_list + ["--device", device, "--seed", "4"]
+
 ]
 
-with futures.ProcessPoolExecutor(max_workers=2) as executor:
+with futures.ProcessPoolExecutor(max_workers=8) as executor:
     results = executor.map(subprocess.call, processes)
 
 
