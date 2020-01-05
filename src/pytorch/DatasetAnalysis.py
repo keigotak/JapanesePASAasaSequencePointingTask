9e3536c0f4bf154341b77aba510887ae70521396
romth <roomtheater@gmail.com>
2020-01-05 23:28:09 +0900

    feat: add results

--- src/pytorch/DatasetAnalysis.py
+++ src/pytorch/DatasetAnalysis.py
@@ -11,12 +11,15 @@ TRAIN = "train"
 DEV = "dev"
 TEST = "test"
 
+with_bccwj = False
+with_bert = True
+
 # train_label, train_args, train_preds, train_prop, train_vocab, train_word_pos, train_ku_pos, train_modes, train_word_pos_id, train_ku_pos_id, train_modes_id = get_datasets_in_sentences_test(TRAIN)
 # dev_label, dev_args, dev_preds, dev_prop, dev_vocab, dev_word_pos, dev_ku_pos, dev_modes, dev_word_pos_id, dev_ku_pos_id, dev_modes_id = get_datasets_in_sentences_test(DEV)
 # test_label, test_args, test_preds, test_prop, test_vocab, test_word_pos, test_ku_pos, test_modes, test_word_pos_id, test_ku_pos_id, test_modes_id = get_datasets_in_sentences_test(TEST)
-train_label, train_args, train_preds, train_prop, train_vocab, train_word_pos, train_ku_pos, train_modes, train_word_pos_id, train_ku_pos_id, train_modes_id = get_datasets_in_sentences(TRAIN)
-dev_label, dev_args, dev_preds, dev_prop, dev_vocab, dev_word_pos, dev_ku_pos, dev_modes, dev_word_pos_id, dev_ku_pos_id, dev_modes_id = get_datasets_in_sentences(DEV)
-test_label, test_args, test_preds, test_prop, test_vocab, test_word_pos, test_ku_pos, test_modes, test_word_pos_id, test_ku_pos_id, test_modes_id = get_datasets_in_sentences(TEST)
+train_label, train_args, train_preds, train_prop, train_vocab, train_word_pos, train_ku_pos, train_modes, train_word_pos_id, train_ku_pos_id, train_modes_id = get_datasets_in_sentences(TRAIN, with_bccwj=with_bccwj, with_bert=with_bert)
+dev_label, dev_args, dev_preds, dev_prop, dev_vocab, dev_word_pos, dev_ku_pos, dev_modes, dev_word_pos_id, dev_ku_pos_id, dev_modes_id = get_datasets_in_sentences(DEV, with_bccwj=with_bccwj, with_bert=with_bert)
+test_label, test_args, test_preds, test_prop, test_vocab, test_word_pos, test_ku_pos, test_modes, test_word_pos_id, test_ku_pos_id, test_modes_id = get_datasets_in_sentences(TEST, with_bccwj=with_bccwj, with_bert=with_bert)
 
 def get_unique_list(seq):
     seen = []
@@ -92,6 +95,7 @@ print('total: {}| total ni_dep: {} | ni_dep: {} / ni_rentai:  {} | ni_zero: {}'.
 
 
 '''
+* ntc
 [Train] number of sentence: 23218 / number of predicates:  62489 / number of ga: 49161 / number of wo: 26799 / number of ni: 6214 / number of else: 1903594
 [Dev] number of sentence: 4628 / number of predicates:  12724 / number of ga: 10072 / number of wo: 5499 / number of ni: 1749 / number of else: 397177
 [Test] number of sentence: 8816 / number of predicates:  23981 / number of ga: 18994 / number of wo: 10315 / number of ni: 2758 / number of else: 742462
@@ -107,4 +111,56 @@ total: 1749| total ni_dep: 1637 | ni_dep: 1565 / ni_rentai:  72 | ni_zero: 112
 total: 18994| total ga_dep: 14230 | ga_dep: 10793 / ga_rentai:  3437 | ga_zero: 4764
 total: 10315| total wo_dep: 9532 | wo_dep: 8861 / wo_rentai:  671 | wo_zero: 783
 total: 2758| total ni_dep: 2547 | ni_dep: 2408 / ni_rentai:  139 | ni_zero: 211
+
+* ntc bert
+[Train] number of sentence: 23218 / number of predicates:  62489 / number of ga: 49161 / number of wo: 26799 / number of ni: 6214 / number of else: 1903594
+[Dev] number of sentence: 4628 / number of predicates:  12724 / number of ga: 10072 / number of wo: 5499 / number of ni: 1749 / number of else: 397177
+[Test] number of sentence: 8816 / number of predicates:  23981 / number of ga: 18994 / number of wo: 10315 / number of ni: 2758 / number of else: 742462
+[Train]
+total: 49161| total ga_dep: 37615 | ga_dep: 28498 / ga_rentai:  9117 | ga_zero: 11546
+total: 26799| total wo_dep: 24997 | wo_dep: 23183 / wo_rentai:  1814 | wo_zero: 1802
+total: 6214| total ni_dep: 5855 | ni_dep: 5645 / ni_rentai:  210 | ni_zero: 359
+[Dev]
+total: 10072| total ga_dep: 7520 | ga_dep: 5793 / ga_rentai:  1727 | ga_zero: 2552
+total: 5499| total wo_dep: 5105 | wo_dep: 4704 / wo_rentai:  401 | wo_zero: 394
+total: 1749| total ni_dep: 1637 | ni_dep: 1565 / ni_rentai:  72 | ni_zero: 112
+[Test]
+total: 18994| total ga_dep: 14230 | ga_dep: 10793 / ga_rentai:  3437 | ga_zero: 4764
+total: 10315| total wo_dep: 9532 | wo_dep: 8861 / wo_rentai:  671 | wo_zero: 783
+total: 2758| total ni_dep: 2547 | ni_dep: 2408 / ni_rentai:  139 | ni_zero: 211
+
+* bccwj
+[Train] number of sentence: 30056 / number of predicates:  78268 / number of ga: 56391 / number of wo: 31580 / number of ni: 13628 / number of else: 2597212
+[Dev] number of sentence: 6373 / number of predicates:  15430 / number of ga: 10779 / number of wo: 6054 / number of ni: 2839 / number of else: 472432
+[Test] number of sentence: 5809 / number of predicates:  14582 / number of ga: 10312 / number of wo: 5775 / number of ni: 2430 / number of else: 491575
+[Train]
+total: 56391| total ga_dep: 41937 | ga_dep: 30735 / ga_rentai:  11202 | ga_zero: 14454
+total: 31580| total wo_dep: 28388 | wo_dep: 26040 / wo_rentai:  2348 | wo_zero: 3192
+total: 13628| total ni_dep: 12501 | ni_dep: 11812 / ni_rentai:  689 | ni_zero: 1127
+[Dev]
+total: 10779| total ga_dep: 8049 | ga_dep: 6107 / ga_rentai:  1942 | ga_zero: 2730
+total: 6054| total wo_dep: 5351 | wo_dep: 4926 / wo_rentai:  425 | wo_zero: 703
+total: 2839| total ni_dep: 2588 | ni_dep: 2446 / ni_rentai:  142 | ni_zero: 251
+[Test]
+total: 10312| total ga_dep: 7623 | ga_dep: 5648 / ga_rentai:  1975 | ga_zero: 2689
+total: 5775| total wo_dep: 5143 | wo_dep: 4702 / wo_rentai:  441 | wo_zero: 632
+total: 2430| total ni_dep: 2171 | ni_dep: 2054 / ni_rentai:  117 | ni_zero: 259
+
+* bccwj bert
+[Train] number of sentence: 30054 / number of predicates:  78223 / number of ga: 56355 / number of wo: 31558 / number of ni: 13619 / number of else: 2586739
+[Dev] number of sentence: 6372 / number of predicates:  15426 / number of ga: 10777 / number of wo: 6052 / number of ni: 2839 / number of else: 471513
+[Test] number of sentence: 5808 / number of predicates:  14550 / number of ga: 10282 / number of wo: 5764 / number of ni: 2424 / number of else: 483295
+[Train]
+total: 56355| total ga_dep: 41919 | ga_dep: 30718 / ga_rentai:  11201 | ga_zero: 14436
+total: 31558| total wo_dep: 28372 | wo_dep: 26026 / wo_rentai:  2346 | wo_zero: 3186
+total: 13619| total ni_dep: 12494 | ni_dep: 11806 / ni_rentai:  688 | ni_zero: 1125
+[Dev]
+total: 10777| total ga_dep: 8048 | ga_dep: 6107 / ga_rentai:  1941 | ga_zero: 2729
+total: 6052| total wo_dep: 5349 | wo_dep: 4924 / wo_rentai:  425 | wo_zero: 703
+total: 2839| total ni_dep: 2588 | ni_dep: 2446 / ni_rentai:  142 | ni_zero: 251
+[Test]
+total: 10282| total ga_dep: 7611 | ga_dep: 5640 / ga_rentai:  1971 | ga_zero: 2671
+total: 5764| total wo_dep: 5134 | wo_dep: 4694 / wo_rentai:  440 | wo_zero: 630
+total: 2424| total ni_dep: 2165 | ni_dep: 2048 / ni_rentai:  117 | ni_zero: 259
+
 '''
--- src/utils/DataDistribution.py
+++ src/utils/DataDistribution.py
@@ -6,9 +6,13 @@ import pickle
 
 
 print('Loading data...')
-train_path = str(Path('../../data/NTC_dataset').joinpath('ided_train.pkl'))
-test_path = str(Path('../../data/NTC_dataset').joinpath('ided_test.pkl'))
-dev_path = str(Path('../../data/NTC_dataset').joinpath('ided_dev.pkl'))
+# train_path = str(Path('../../data/NTC_dataset').joinpath('ided_train2_bert.pkl'))
+# test_path = str(Path('../../data/NTC_dataset').joinpath('ided_test_bert.pkl'))
+# dev_path = str(Path('../../data/NTC_dataset').joinpath('ided_dev_bert.pkl'))
+train_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('ided_train_bccwj_bert.pkl'))
+test_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('ided_test_bccwj_bert.pkl'))
+dev_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('ided_dev_bccwj_bert.pkl'))
+
 with open(train_path, 'rb') as f:
     train_data = pickle.load(f)
 with open(test_path, 'rb') as f:
@@ -28,14 +32,58 @@ def count_freq(_tag, _data):
     print(np.asarray((unique_elements, counts_elements)))
     print(np.round(counts_elements / sum_counts * 100, 2))
 
-count_freq('train', y_train)
-print("----------")
-count_freq('test', y_test)
-print("----------")
-count_freq('dev', y_dev)
+# count_freq('train', y_train)
+# print("----------")
+# count_freq('test', y_test)
+# print("----------")
+# count_freq('dev', y_dev)
 
+print('Loading data...')
+# train_path = str(Path('../../data/NTC_dataset').joinpath('listed_train2.pkl'))
+# test_path = str(Path('../../data/NTC_dataset').joinpath('listed_test.pkl'))
+# dev_path = str(Path('../../data/NTC_dataset').joinpath('listed_dev.pkl'))
+# train_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_train_bccwj.pkl'))
+# test_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_test_bccwj.pkl'))
+# dev_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_dev_bccwj.pkl'))
+train_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_train_bccwj_bert.pkl'))
+test_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_test_bccwj_bert.pkl'))
+dev_path = str(Path('../../data/BCCWJ-DepParaPAS/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/BCCWJ-DepParaPAS-3.3.0_1.2.0_20160301/ntc').joinpath('listed_dev_bccwj_bert.pkl'))
+
+with open(train_path, 'rb') as f:
+    train_data = pickle.load(f)
+with open(test_path, 'rb') as f:
+    test_data = pickle.load(f)
+with open(dev_path, 'rb') as f:
+    dev_data = pickle.load(f)
+
+
+def get_property(_data):
+    sent_lens = {}
+    cnt = 0
+    sent_id = _data[0][6]
+    sent_len = 0
+    for item in _data:
+        if item[6] > sent_id:
+            cnt += 1
+            sent_id = item[6]
+            if sent_len in sent_lens.keys():
+                sent_lens[sent_len] += 1
+            else:
+                sent_lens[sent_len] = 1
+        sent_len = item[7]
+    cnt += 1
+    if sent_len in sent_lens.keys():
+        sent_lens[sent_len] += 1
+    else:
+        sent_lens[sent_len] = 1
+    return {'details': sent_lens, 'total': cnt}
+
+print('total number of sentences train: {}'.format(get_property(train_data)['total']))
+print('total number of sentences test: {}'.format(get_property(test_data)['total']))
+print('total number of sentences dev: {}'.format(get_property(dev_data)['total']))
 
 '''
+* ntc / ntc with BERT
 Frequency of unique values of the said array: train
 [[      0       1       2       3]
  [  50890   27216    6701 2102725]]
@@ -50,6 +98,48 @@ Frequency of unique values of the said array: dev
 [[     0      1      2      3]
  [ 10121   5532   1850 437920]]
 [ 2.22  1.21  0.41 96.16]
+total number of sentences train: 62489
+total number of sentences test: 23981
+total number of sentences dev: 12724
+
+* bccwj
+Frequency of unique values of the said array: train
+[[      0       1       2       3]
+ [  57982   32065   14091 2858121]]
+[ 1.96  1.08  0.48 96.48]
+----------
+Frequency of unique values of the said array: test
+[[     0      1      2      3]
+ [ 10626   5883   2504 542973]]
+[ 1.89  1.05  0.45 96.62]
+----------
+Frequency of unique values of the said array: dev
+[[     0      1      2      3]
+ [ 11093   6110   2931 525829]]
+[ 2.03  1.12  0.54 96.31]
+total number of sentences train: 78268
+total number of sentences test: 14582
+total number of sentences dev: 15430
+
+
+* bccwj with BERT
+Frequency of unique values of the said array: train
+[[      0       1       2       3]
+ [  57958   32049   14089 2779895]]
+[ 2.01  1.11  0.49 96.39]
+----------
+Frequency of unique values of the said array: test
+[[     0      1      2      3]
+ [ 10619   5873   2503 528409]]
+[ 1.94  1.07  0.46 96.53]
+----------
+Frequency of unique values of the said array: dev
+[[     0      1      2      3]
+ [ 11079   6106   2929 510419]]
+[ 2.09  1.15  0.55 96.21]
+total number of sentences train: 78223
+total number of sentences test: 14550
+total number of sentences dev: 15426
 '''
 
 
