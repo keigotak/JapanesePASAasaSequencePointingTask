a73f0e524e824e91d411a0b62023a64d32fdbd05
romth <roomtheater@gmail.com>
2020-03-08 15:22:03 +0900

    feat: add index

--- src/pytorch/CombinePrediction.py
+++ src/pytorch/CombinePrediction.py
@@ -67,13 +67,13 @@ def main(tag):
                 ref = Path("../../results/packed/sentences_bccwj.txt")
             with ref.open("r", encoding="utf-8") as f:
                 sentences = [line.strip().split(',') for line in f.readlines()]
-            log = [i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] + [s[-2]] + ['False'] for i1, i2, i3, i4, i5, s in
-                   zip(data1, data2, data3, data4, data5, sentences)]
+            log = [[i] + i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] + [s[-2]] + ['False'] for i, (i1, i2, i3, i4, i5, s) in
+                   enumerate(zip(data1, data2, data3, data4, data5, sentences))]
         else:
-            log = [i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] for
-                   i1, i2, i3, i4, i5 in
-                   zip(data1, data2, data3, data4, data5)]
-        log = pd.DataFrame(log, columns=["arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sl1", "sl2", "sl3", "sl4", "sl5", "counts", "sentence", "conflict"])
+            log = [[i] + i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] for
+                   i, (i1, i2, i3, i4, i5) in
+                   enumerate(zip(data1, data2, data3, data4, data5))]
+        log = pd.DataFrame(log, columns=["index", "arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sl1", "sl2", "sl3", "sl4", "sl5", "counts", "sentence", "conflict"])
 
         item1 = log.sl1.values
         item2 = log.sl2.values
@@ -107,10 +107,11 @@ def main(tag):
         with log5.open("r", encoding="utf-8") as f:
             data5 = [line.strip().split(',') for line in f.readlines()]
 
-        log = [i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] for
-               i1, i2, i3, i4, i5 in
-               zip(data1, data2, data3, data4, data5)]
-        log = pd.DataFrame(log, columns=["arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sl1", "sl2", "sl3", "sl4", "sl5", "counts", "sentence", "conflict"])
+        log = [[i] + i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:]
+               for
+               i, (i1, i2, i3, i4, i5) in
+               enumerate(zip(data1, data2, data3, data4, data5))]
+        log = pd.DataFrame(log, columns=["index", "arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sl1", "sl2", "sl3", "sl4", "sl5", "counts", "sentence", "conflict"])
 
         item1 = log.sl1.values
         item2 = log.sl2.values
@@ -212,10 +213,11 @@ def main(tag):
         with log5.open("r", encoding="utf-8") as f:
             data5 = [line.strip().split(',') for line in f.readlines()]
 
-        log = [i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] for
-               i1, i2, i3, i4, i5 in
-               zip(data1, data2, data3, data4, data5)]
-        log = pd.DataFrame(log, columns=["arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sp1", "sp2", "sp3", "sp4", "sp5", "counts", "sentence", "conflict"])
+        log = [[i] + i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:]
+               for
+               i, (i1, i2, i3, i4, i5) in
+               enumerate(zip(data1, data2, data3, data4, data5))]
+        log = pd.DataFrame(log, columns=["index", "arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sp1", "sp2", "sp3", "sp4", "sp5", "counts", "sentence", "conflict"])
 
         item1 = log.sp1.values
         item2 = log.sp2.values
@@ -296,10 +298,11 @@ def main(tag):
                     break
             summary_conflict.append(ans)
 
-        log = [i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:] for
-               i1, i2, i3, i4, i5 in
-               zip(data1, data2, data3, data4, data5)]
-        log = pd.DataFrame(log, columns=["arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sp1", "sp2", "sp3", "sp4", "sp5", "counts", "sentence", "conflict"])
+        log = [[i] + i1[:8] + [i2[7], i3[7], i4[7], i5[7], [i1[7], i2[7], i3[7], i4[7], i5[7]].count(i1[6])] + i1[8:]
+               for
+               i, (i1, i2, i3, i4, i5) in
+               enumerate(zip(data1, data2, data3, data4, data5))]
+        log = pd.DataFrame(log, columns=["index", "arg", "pred", "prop", "word_distance", "ku_distance", "pred_or_not", "label", "sp1", "sp2", "sp3", "sp4", "sp5", "counts", "sentence", "conflict"])
 
         item1 = log.sp1.values
         item2 = log.sp2.values
